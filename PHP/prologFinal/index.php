084d9c9a5ec65250ff51d2bc5a23e7031c0a6263
MT <manutm24@gmail.com>
2017-07-24 23:54:21 -0400

    Documento

--- PHP/prologFinal/index.php
+++ PHP/prologFinal/index.php
@@ -21,7 +21,7 @@ switch($_GET['funcion']) {
  function preguntas($nombreEnfermedad){
 	//echo $nombreEnfermedad;
 	//$preguntas = 'C:\progra~2\swipl\bin\swipl.exe -s C:\Users\MT\IdeaProjects\prologFinal\prolog.pl -g test,halt';
-    $command= `swipl -s C:\Users\MT\IdeaProjects\prologFinalPHP\prolog.pl -g "enfermedad_sintoma($nombreEnfermedad). -t 'halt'"`;
+    $command= `swipl -s C:\Users\MT\IdeaProjects\prologFinal_JAVAPHP\prolog.pl -g "enfermedad_sintoma($nombreEnfermedad). -t 'halt'"`;
 	exec($command, $preguntas);
 	echo $command;
 	
@@ -37,7 +37,7 @@ switch($_GET['funcion']) {
  function insertarEnfermedad($nombreEnfermedad){
 	echo $nombreEnfermedad;
 	//$preguntas = 'C:\progra~2\swipl\bin\swipl.exe -s C:\Users\MT\IdeaProjects\prologFinal\prolog.pl -g test,halt';
-    $command= `swipl -s C:\Users\MT\IdeaProjects\prologFinalPHP\prolog.pl -g "insertarEnfermedad($nombreEnfermedad)). -t 'halt'"`;
+    $command= `swipl -s C:\Users\MT\IdeaProjects\prologFinal_JAVAPHP\prolog.pl -g "insertarEnfermedad($nombreEnfermedad)). -t 'halt'"`;
 	exec($command, $preguntas);
 	echo "INSERTADO";
 	
